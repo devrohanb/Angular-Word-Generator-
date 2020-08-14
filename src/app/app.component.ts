376d5c70d2b698822ca94d33d499d601731a1f04
Rohan Bari <dev.rohanb@gmail.com>
2020-08-14 21:28:06 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import arrayWords from '../utils/words';
+import arrayCountries from '../utils/countries';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+  countries = '';
+  num = 5;
+
+  // Handling the slider change listener:
+  handleSliderChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  // Generating words on the basis of limit from arraWords
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
+
+  generateCountries() {
+    this.num = Math.floor(Math.random() * 16) + 1;
+    this.countries = arrayCountries.slice(0, this.num).join(' ');
+  }
 }
---
+++ src/utils/countries.ts
@@ -0,0 +1,18 @@
+export default [
+  'India',
+  'America',
+  'Russia',
+  'Pakistan',
+  'China',
+  'Japan',
+  'Bangladesh',
+  'Nepal',
+  'Shri-Lanka',
+  'Afganistan',
+  'Balochistan',
+  'London',
+  'France',
+  'Italy',
+  'Bhutan',
+  'Africa',
+];
